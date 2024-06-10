0ea2e3f45cc5301ad043d8dd580ddfda041bc41e
Amam Khokhar <amamqaiser@gmail.com>
2024-06-10 17:59:59 +0500

    Update application.properties file

--- src/main/java/com/learner/jobms/job/clients/CompanyClient.java
+++ src/main/java/com/learner/jobms/job/clients/CompanyClient.java
@@ -5,8 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="COMPANY-SERVICE"
-        , url = "${company-service.url}")
+@FeignClient(name="COMPANY-SERVICE")
 public interface CompanyClient {
 
     @GetMapping("/company/{id}")
--- src/main/java/com/learner/jobms/job/clients/ReviewClient.java
+++ src/main/java/com/learner/jobms/job/clients/ReviewClient.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name="REVIEW-SERVICE"
-        , url = "${review-service.url}")
+@FeignClient(name="REVIEW-SERVICE")
 public interface ReviewClient {
 
     @GetMapping("/reviews" )
